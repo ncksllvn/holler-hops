4b06399759368e3ffc5789d338b43d08a54c0952
nick <ncksllvn@gmail.com>
2015-11-20 00:03:02 -0500

    Starting style

--- public/javascripts/init.js
+++ public/javascripts/init.js
@@ -2,6 +2,16 @@
   $(function(){
 
     $('.button-collapse').sideNav();
+    
+    $(document).ready(function(){
+      $('.slider').slider({full_width: true});
+    });
+    
+    $('.maps').click(function () {
+      $('.maps iframe').css("pointer-events", "auto");
+    });
+
+        
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
