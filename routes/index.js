5ccb75d61bad3903075c94d5b6a6f75e3d59736e
nick <ncksllvn@gmail.com>
2015-11-20 16:50:25 -0500

    Adding routes, views, babel

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', (req, res, next) => 
+  res.render('index', { title: 'Home' })
+)
 
-module.exports = router;
+router.get('/tap', (req, res, next) => 
+  res.render('tap', { title: 'On Tap' })
+)
+
+router.get('/menu', (req, res, next) => 
+  res.render('menu', { title: 'Menu' })
+)
+
+router.get('/story', (req, res, next) => 
+  res.render('story', { title: 'Our Story' })
+)
+
+router.get('/contact', (req, res, next) =>
+  res.render('contact', { title: 'Contact Us' })
+)
+
+module.exports = router
