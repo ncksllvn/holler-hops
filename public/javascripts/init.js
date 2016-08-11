0a5224954f2a456be6a979bee6a259f707faf3d2
nick <ncksllvn@gmail.com>
2016-08-11 11:36:37 -0400

    add a fail handler for contact page

--- public/javascripts/init.js
+++ public/javascripts/init.js
@@ -3,43 +3,47 @@
 
     // Side navigation for mobile ------------
     $('.button-collapse').sideNav();
-    
+
     // Dropdown buttons
     $(".dropdown-button").dropdown();
-    
+
     // Index page -------------------------
     $('.slider').slider({full_width: true});
-    
+
     // Fix for Google Map picking up scrolling
     $('.maps').click(function () {
       $('.maps iframe').css("pointer-events", "auto");
     });
-    
+
     // Menu page ------------------------
-    var $menu = $('#menu') 
-    
+    var $menu = $('#menu')
+
     if ($menu.length)
       $('#menu ul').pushpin({ top: $menu.offset().top, offset: 64 });
-    
+
     $('.scrollspy').scrollSpy();
-    
+
     // Our Story --------------------------
     $('.parallax').parallax();
-    
+
     // Contact Page ------------------------
     $('form[name=contact]').submit(function(event){
       event.preventDefault()
-      
+
       var $this = $(this)
       var $submit = $this.find('button[type=submit]').attr('disabled', 'true')
       var $message = $('.email-response')
-      
+
       $.post('/contact', $this.serialize())
-        .always(function(response){
-        $this.fadeOut()
-        $message.html('Your message has been received! We will respond to you soon.')
-      })
-      
+        .done(function(response){
+          $this.fadeOut()
+          $message.html('Your message has been received! We will respond to you soon.')
+        })
+        .fail(function(response){
+          $message.html('There was a problem trying to send your message. Sorry about that.')
+        })
+
+
     })
 
   }); // end of document ready
