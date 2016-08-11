bc708ff45890e96244c37e06115e5d8fd12e3e35
nick <ncksllvn@gmail.com>
2016-08-11 11:21:50 -0400

    switch to mailgun

--- util/email.js
+++ util/email.js
@@ -1,18 +1,20 @@
 var nodemailer = require('nodemailer')
+var mg = require('nodemailer-mailgun-transport')
 var sendToEmail = process.env.SEND_TO_EMAIL
-var email = process.env.EMAIL_ADDRESS
-var password = process.env.EMAIL_ADDRESS_PASSWORD
+//var email = process.env.EMAIL_ADDRESS
+//var password = process.env.EMAIL_ADDRESS_PASSWORD
+var apiKey = process.env.MAILGUN_API_KEY
 var transporter
+var auth = {
+  auth: {
+    api_key: apiKey,
+    domain: 'hollerhops.com'
+  }
+}
 
-if (email)
+if (apiKey)
 {
-  transporter = nodemailer.createTransport({
-      service: 'Gmail',
-      auth: {
-          user: email,
-          pass: password
-      }
-  });
+  transporter = nodemailer.createTransport(mg(auth));
 }
 else
 {
@@ -20,11 +22,11 @@ else
     sendMail(options, callback){
       setTimeout(callback, 300, null, { message: 'In development mode - no email was sent.'})
     }
-  }  
-} 
+  }
+}
 
 module.exports = (data, callback) => {
-  
+
   var html = `
       <table>
       <tr>
@@ -39,22 +41,24 @@ module.exports = (data, callback) => {
       <tr>
       <td><label>Message:</label></td><td>${data.message}</td>
     </table>`
-  
+
   var text = `
     Name: ${data.name}\n
     Email: ${data.email}\n
     Phone: ${data.phone}\n
     Message: ${data.message}`
-    
+
   var mailOptions = {
       from: 'Holler Hops <donotreply@hollerhops.com>', // sender address
       to: sendToEmail, // list of receivers
       subject: 'You have a new message', // Subject line
       text: text,
       html: html
-  };
-  
-  transporter.sendMail(mailOptions, callback);
-  
+  }
+
+  console.log('Sending mail - ' + text)
+
+  transporter.sendMail(mailOptions, callback)
+
 }
 
